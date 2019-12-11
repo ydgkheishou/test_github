727a2751ff81901d1f4de23d0e13e24e6ff82684
asus <1669694189@qq.com>
2019-12-12 00:20:41 +0800

    这是我第一次提交的idea代码

---
+++ src/com/ydgk/TestClass.java
@@ -0,0 +1,10 @@
+package com.ydgk;
+
+/**
+ * @author asus
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("这是测试idea提交程序到github远程库的程序！");
+    }
+}
